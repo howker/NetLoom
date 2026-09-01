9149c88536f829acb68c257d9a4e05f3f69dc80c
howker <howker2007@gmail.com>
2026-09-01 23:01:05 +0400

    аркас: добавлена основная архитектура NetLoom

---
+++ tools/NetLoom.DbTool/Program.cs
@@ -0,0 +1,9 @@
+namespace NetLoom.DbTool
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ tools/NetLoom.Simulator/Program.cs
@@ -0,0 +1,9 @@
+namespace NetLoom.Simulator
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
